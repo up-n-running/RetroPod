386a3e9322cdba9c39dee0972f340d6fdfa6f3fc
John Milner <john@upnrunning.co.uk>
2020-05-15 01:43:44 +0100

    Added windows build script batch file

---
+++ retropod/windows_compile_src.bat
@@ -0,0 +1,6 @@
+REM g++ -o ./dist_mac/RetroPod ./src/surface.cpp ./src/shadow.cpp ./src/Node.cpp ./src/Model.cpp ./src/Hovercraft.cpp ./src/Checkpoint.cpp ./src/Main.cpp -lglu32 -lopengl32 -framework GLUT -framework OpenGL -D GL_SILENCE_DEPRECATION
+REM Install Cygwin from here: http://www.cygwin.com/    (only 2 optional modules you need to select are: g++ and mingw64-x86_64-freeglut)
+REM Add C:\cygwin\bin to windows PATH environment variable
+REM you should be able to run g++ from cmd now
+
+g++ -o ./dist_mac/RetroPod ./src/surface.cpp ./src/shadow.cpp ./src/Node.cpp ./src/Model.cpp ./src/Hovercraft.cpp ./src/Checkpoint.cpp ./src/Main.cpp -lglu32 -lopengl32 -D GL_SILENCE_DEPRECATION -I. -L.
